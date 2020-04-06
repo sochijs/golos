3e22aa612249eaea5b403ebc7b2c78d7e38ce628
Sergey Startsev <sergey@webstartsev.ru>
2020-04-06 14:11:45 +0300

    init proj

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {useNewUrlParser: true});
+
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}...`);
+    });
+  } catch (e) {
+    console.log('Server error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
+
