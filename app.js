01a430f4090451b6b0b8a11eeff660d67b01d4b4
Sergey Startsev <sergey@webstartsev.ru>
2020-04-16 15:44:47 +0300

    app to server

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/api/vote', require('./routes/vote.routes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build/'));
 
-  app.get('*', (req, res) => {
+  server.get('*', (req, res) => {
     res.sendFile(path.resolve(
       __dirname, 'client', 'build', 'index.html'
     ));
